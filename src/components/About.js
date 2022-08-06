94b0d8b814f76c28bb3fb59a64f1d6f12331d5b0
swapnil <swapnilvyas18@gmail.com>
2022-08-06 11:57:55 +0530

    Version 1

---
+++ src/components/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function About(props) {
+  let myStyle = {
+    color: props.mode === "dark" ? "white" : "#042743",
+    backgroundColor: props.mode === "dark" ? "rgb(36 74 104)" : "white",
+  };
+  return (
+    <div className="container">
+      <h2 className="my-3" style={{ color: props.mode === "dark" ? "white" : "#042743" }}>
+        About Us
+      </h2>
+      <div className="accordion" id="accordionExample" style={myStyle}>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingOne">
+            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
+              <strong>Analyze Your Text</strong>
+            </button>
+          </h2>
+          <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div className="accordion-body" style={myStyle}>
+              TextUtils gives you a way to analyze your text quickly and effeciently. Be it word count, charactercount or
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingTwo">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style={myStyle}>
+              <strong>Free to use</strong>
+            </button>
+          </h2>
+          <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+            <div className="accordion-body" style={myStyle}>
+              TextUtils is a free character counter tool that provides instant character count and word count statistics for a given text. TextUtils reports the number of words and characters. This is
+              suitable for writing text with word/character limit.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="headingThree">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style={myStyle}>
+              <strong>Browser Compatible</strong>
+            </button>
+          </h2>
+          <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+            <div className="accordion-body" style={myStyle}>
+              This word counter software works in any web browser.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/TextForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase!", "success");
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase!", "success");
+  };
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared!", "success");
+  };
+  const handleCopyClick = () => {
+    let text = document.getElementById("myBox");
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Copied to Clipboard!", "success");
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/); /*Js Rejects*/
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces removed!", "success");
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div className="container" style={{ color: props.mode === "light" ? "black" : "white" }}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{ backgroundColor: props.mode === "light" ? "white" : "#13466e", color: props.mode === "light" ? "black" : "white" }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick} disabled={text.length === 0}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick} disabled={text.length === 0}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick} disabled={text.length === 0}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCopyClick} disabled={text.length === 0}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces} disabled={text.length === 0}>
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div className="container my-3" style={{ color: props.mode === "light" ? "black" : "white" }}>
+        <h2>Your text summary</h2>
+        <p>
+          {
+            text.split(" ").filter((item) => {
+              return item !== "";
+            }).length
+          }{" "}
+          words, {text.length}, characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((item) => {
+              return item !== "";
+            }).length}{" "}
+          minutes read
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
+      </div>
+    </>
+  );
+}
